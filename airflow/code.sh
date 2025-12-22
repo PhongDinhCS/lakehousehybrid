d5668c24b973f50d58ed8ff364b000013d002385
PhongDinhCS <128981139+PhongDinhCS@users.noreply.github.com>
2025-12-22 20:43:22 +0700

    Update code.sh

--- airflow/code.sh
+++ airflow/code.sh
@@ -6,5 +6,11 @@
     --role Admin \
     --email admin@example.com
 
+docker exec -it --user root airflow-webserver bash
+chmod 666 /var/run/docker.sock
+exit
+
 
 docker exec -it airflow-webserver airflow scheduler
+
+
